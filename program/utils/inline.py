b7727e718f588d26ebca03301b238eb04cdb01a4
Jaaxson <99959177+Jaaxson@users.noreply.github.com>
2022-03-17 11:14:55 +0200

    Update inline.py

--- program/utils/inline.py
+++ program/utils/inline.py
@@ -17,7 +17,7 @@ def stream_markup(user_id):
     [
     InlineKeyboardButton(
                         "♡اضـف الـبـوت لـمـجـمـوعـتـك♡",
-                        url=f'https://t.me/{BOT_USERNAME}?startgroup=true'),
+                        url=f'https://t.me/{HO_mu_1bot}?startgroup=true'),
     ],
   ]
   return buttons
